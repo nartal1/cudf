8cdfd001b67eabe8f1bd9d7842d37d8bdeb153d0
gashen <gashen@nvidia.com>
2019-06-20 14:23:10 +0800

    Use ENV to define include path

--- ci/gpu/build-centos7.sh
+++ ci/gpu/build-centos7.sh
@@ -16,21 +16,24 @@ git clone --recurse-submodules https://github.com/rapidsai/custrings.git
 gcc --version
 
 export CUDACXX=/usr/local/cuda/bin/nvcc
+export INSTALL_PREFIX=/usr/local/rapids
 
 mkdir -p /rapids/rmm/build
 cd /rapids/rmm/build
-cmake .. -DCMAKE_CXX11_ABI=OFF
+cmake .. -DCMAKE_CXX11_ABI=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
 make -j4 install
 
 mkdir -p /rapids/custrings/cpp/build
 cd /rapids/custrings/cpp/build
-cmake .. -DCMAKE_CXX11_ABI=OFF
+cmake .. -DCMAKE_CXX11_ABI=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
 make -j4 install
 
 mkdir -p $WORKDIR/cpp/build
 cd $WORKDIR/cpp/build
+export RMM_ROOT=$INSTALL_PREFIX
+export NVSTRINGS_ROOT=$INSTALL_PREFIX
 cmake .. -DCMAKE_CXX11_ABI=OFF
-make -j4 install
+make -j4 install DESTDIR=$INSTALL_PREFIX
 
 cd $WORKDIR/java
 mvn -P abiOff package
